fe9d58a3582206e739fed367b5aa0c3b150cec55
LauraMilenaRB <lauramilena.rbe@gmail.com>
2021-11-10 23:59:57 -0500

    final commit

--- ScriptDespliegueAtomaticoAWS.sh
+++ ScriptDespliegueAtomaticoAWS.sh
@@ -7,16 +7,16 @@
 	#3. Añade los permisos al grupo de seguridad para recibir conexiones y peticiones sobre los puertos 22 y 8080.
 	aws ec2 authorize-security-group-ingress --group-name sg-AppWebAWS --protocol tcp --port 8080 --cidr 0.0.0.0/0
 	aws ec2 authorize-security-group-ingress --group-name sg-AppWebAWS --protocol tcp --port 22 --cidr 0.0.0.0/0
-	#Crea las 3 instancias EC2.
-	aws ec2 run-instances --image-id ami-032930428bf1abbff --count 3 --instance-type t2.micro --key-name claveAppWebAWS --security-groups sg-cli-AppWebAWS
+	#4. Crea las 3 instancias EC2.
+	aws ec2 run-instances --image-id ami-032930428bf1abbff --count 3 --instance-type t2.micro --key-name claveAppWebAWS --security-groups sg-AppWebAWS
 	sleep 100
-	# Consulta la informacion de las instacias creadas y escribe la salida en un archivo json llamado 'infoinstancias.json'
+	#5. Consulta la informacion de las instacias creadas y escribe la salida en un archivo json llamado 'infoinstancias.json'
 	aws ec2 describe-instances --filters "Name=instance.group-name,Values=sg-AppWebAWS" --query "Reservations[].Instances[]" > infoinstancias.json
 	for (( numinstance=0; numinstance<3; numinstance++ ))
     do
-      #Por cada instancia se busca el dns en el archivo generado en el paso anterior.
+      #6. Por cada instancia se busca el dns en el archivo generado en el paso anterior.
       dns=$(./jq-win64.exe -r '.['$numinstance'].PublicDnsName' infoinstancias.json)
-      #Se conecta por ssh a cada instancia, para descargar, iniciar, y ejecutar la imagen desplegada en dockerhub.
+      #7. Se conecta por ssh a cada instancia, para descargar, iniciar, y ejecutar la imagen desplegada en dockerhub.
       ssh -o StrictHostKeyChecking=no -i "claveAppWebAWS.pem" ec2-user@$dns "sudo yum update -y"
       ssh -i "claveAppWebAWS.pem" ec2-user@$dns "sudo yum install -y docker"
       ssh -i "claveAppWebAWS.pem" ec2-user@$dns "sudo usermod -a -G docker ec2-user"
